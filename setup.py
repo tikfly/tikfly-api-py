18f39d71d9e2e91e593f08edbd75820e130fa707
bishamon82 <tikflydotio@gmail.com>
2026-01-06 19:44:12 +0700

    v1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setup(
   name='tikfly',
-  version='1.1.0',
+  version='1.1.1',
   author='Tikfly',
   author_email='hello@tikfly.io',
   description='Unofficial TikTok API Wrapped in Python',
